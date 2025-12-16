774a0fbe58d9a954d59375585c7265183dcb442d
Zeyad Mohamed Araft <zeyadarafat833@gmail.com>
2025-12-16 07:40:43 +0200

    updates on RR (not working yet)

--- Main.java
+++ Main.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
     private class Process {
@@ -32,12 +30,16 @@ public class Main {
     }
 
     private class RRScheduler {
-        private Queue<Process> processQueue;
+        private ArrayList<Process> processes;
         private int currentTime;
         private final int quantum;
-        private int numProcesses;
-        public RRScheduler(ArrayList<Process> processes, int quantum) {
-            this.processQueue = new LinkedList<>();
+        private final int contextSwitching;
+        private final double numProcesses;
+        private Vector<String> executionOrder;
+
+        public RRScheduler(ArrayList<Process> processes, int quantum, int contextSwitching) {
+            this.processes = new ArrayList<>(processes);
+            this.executionOrder = new Vector<>();
             // sorting on process arrival time
             this.numProcesses = processes.size();
             boolean swapped;
@@ -53,59 +55,118 @@ public class Main {
                 }
                 if (!swapped) break;
             }
-            processQueue.addAll(processes);
             this.currentTime = 0;
             this.quantum = quantum;
+            this.contextSwitching = contextSwitching;
         }
         public void simulate() {
             Queue<Process> finishedProcesses = new LinkedList<>();
+            Queue<Process> readyProcesses = new LinkedList<>();
             Process nextProcess;
             Process currentProcess = null;
             for (int i = 0; i < numProcesses; i++) {
-                Process temp = processQueue.poll();
+                Process temp = processes.get(i);
                 temp.setTurnAroundTime(temp.getArrivalTime() * (-1));
                 temp.setWaitingTime(temp.getArrivalTime() * (-1));
-                processQueue.add(temp);
+                processes.set(i, temp);
             }
-            while (!processQueue.isEmpty()) {
-                nextProcess = processQueue.peek();
-                if (nextProcess.getArrivalTime() <= currentTime) {
-                    currentProcess = processQueue.poll();
+            for (int i = 0; i < processes.size(); i++) {
+                if (processes.get(i).getArrivalTime() <= currentTime) {
+                    readyProcesses.add(processes.get(i));
+                    processes.remove(i);
+                    i--;
                 }
-                else if (currentProcess != null) {
-                    currentTime = nextProcess.getArrivalTime();
-                    continue;
-                }
-                int currentProcessTime = Math.min(currentProcess.getBurstTime(), quantum);
-                // processing simulation
-                for (int i = 0; i < currentProcessTime; i++) {
-                    System.out.println("Time: " + currentTime + ", processing " + currentProcess.getId());
-                    currentTime++;
-                    currentProcess.setBurstTime(currentProcess.getBurstTime() - 1);
-                    currentProcess.setTurnAroundTime(currentProcess.getTurnAroundTime() + 1);
-                    for (int j = 0; j < processQueue.size(); j++) {
-                        Process temp = processQueue.poll();
-                        temp.setTurnAroundTime(temp.getTurnAroundTime() + 1);
-                        temp.setWaitingTime(temp.getWaitingTime() + 1);
-                        processQueue.add(temp);
+            }
+            while (readyProcesses.isEmpty()){
+                currentTime++;
+                for (int i = 0; i < processes.size(); i++) {
+                    if (processes.get(i).getArrivalTime() <= currentTime) {
+                        readyProcesses.add(processes.get(i));
+                        processes.remove(i);
+                        i--;
                     }
                 }
-                if (!processQueue.isEmpty()) System.out.println("Time: " + currentTime + ", Switching to Process " + processQueue.peek().getId());;
-                if (currentProcess.getBurstTime() > 0) processQueue.add(currentProcess);
-                else finishedProcesses.add(currentProcess);
             }
-            System.out.println("Time: " + currentTime + ", Finished All Processes!!!");
-            int totalWaitingTime = 0;
-            int totalTurnAroundTime = 0;
+
+            while (!readyProcesses.isEmpty()) {
+                currentProcess = readyProcesses.poll();
+                // processing simulation
+                int currentProcessTime = Math.min(currentProcess.getBurstTime(), quantum);
+                for (int j = 0; j < readyProcesses.size(); j++) {
+                    Process temp = readyProcesses.poll();
+                    temp.setTurnAroundTime(temp.getTurnAroundTime() + currentProcessTime + contextSwitching);
+                    temp.setWaitingTime(temp.getWaitingTime() + currentProcessTime + contextSwitching);
+                    readyProcesses.add(temp);
+                }
+                for (int i = 0; i < currentProcessTime; i++){
+                    currentTime++;
+                    for (int j = 0; j < processes.size(); j++) {
+                        if (processes.get(j).getArrivalTime() <= currentTime) {
+                            readyProcesses.add(processes.get(j));
+                            processes.remove(j);
+                            j--;
+                        }
+                    }
+                }
+                executionOrder.add(currentProcess.getId());
+                if (currentProcess.getBurstTime() > 0) readyProcesses.add(currentProcess);
+                else finishedProcesses.add(currentProcess);
+                currentTime += contextSwitching;
+                for (int i = 0; i < processes.size(); i++) {
+                    if (processes.get(i).getArrivalTime() <= currentTime) {
+                        readyProcesses.add(processes.get(i));
+                        processes.remove(i);
+                        i--;
+                    }
+                }
+            }
+            double totalWaitingTime = 0;
+            double totalTurnAroundTime = 0;
+            System.out.print("Execution Order: ");
+            for (String s : executionOrder) {
+                System.out.print(s + " ");
+            }
+            System.out.println();
+            // sorting the processes
+            ArrayList<Process> sortedProcesses = new ArrayList<>(finishedProcesses);
+            boolean swapped;
+            for (int i = 0; i < numProcesses-1; i++) {
+                swapped = false;
+                for (int j = 0; j < numProcesses - 1 - i; j++) {
+                    if (sortedProcesses.get(j).getArrivalTime() > sortedProcesses.get(j+1).getArrivalTime()) {
+                        Process temp = sortedProcesses.get(j);
+                        sortedProcesses.set(j, sortedProcesses.get(j+1));
+                        sortedProcesses.set(j+1, temp);
+                        swapped = true;
+                    }
+                }
+                if (!swapped) break;
+            }
+
             for (int i = 0; i < numProcesses; i++) {
-                Process temp = finishedProcesses.poll();
+                Process temp = sortedProcesses.get(i);
                 totalWaitingTime += temp.getWaitingTime();
                 totalTurnAroundTime += temp.getTurnAroundTime();
                 System.out.println("Process: " + temp.getId() + ", Waiting time: " + temp.getWaitingTime() + ", Turn around time: " + temp.getTurnAroundTime());
             }
-            int avgTurnAroundTime = totalTurnAroundTime / numProcesses;
-            int avgWaitingTime = totalWaitingTime / numProcesses;
+            double avgTurnAroundTime = totalTurnAroundTime / numProcesses;
+            double avgWaitingTime = totalWaitingTime / numProcesses;
             System.out.println("AVG Waiting time: " + avgWaitingTime + ", AVG Turn around time: " + avgTurnAroundTime);
         }
+    }
+    public void main(String[] args) {
+        Process p1 = new Process("p1", 8, 0, 1);
+        Process p2 = new Process("p2", 4, 1, 1);
+        Process p3 = new Process("p3", 2, 2, 1);
+        Process p4 = new Process("p4", 1, 3, 1);
+        Process p5 = new Process("p5", 3, 4, 1);
+        ArrayList<Process> processes = new ArrayList<>();
+        processes.add(p1);
+        processes.add(p2);
+        processes.add(p3);
+        processes.add(p4);
+        processes.add(p5);
+        RRScheduler scheduler = new RRScheduler(processes, 2, 1);
+        scheduler.simulate();
     }
 }
